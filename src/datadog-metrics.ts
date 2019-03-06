725844af6c22f4b461368edf95ee9a33ceb8895f
Vanesa Ortiz <vanesa@sourcegraph.com>
2019-03-06 20:41:56 +0900

    refactor code

--- src/datadog-metrics.ts
+++ src/datadog-metrics.ts
@@ -4,51 +4,42 @@ import * as sourcegraph from 'sourcegraph'
 
 const STATSD_PATTERN = /statsd\.[^\'\"]+\([\'\"]([^\'\"]+)[\'\"]\)*/gi
 
-export function activate(context: sourcegraph.ExtensionContext): void {
-    function decorateEditors(editorsToUpdate: sourcegraph.CodeEditor[]): void {
-        for (const editor of editorsToUpdate) {
-            const decorations: sourcegraph.TextDocumentDecoration[] = []
-            for (const [i, line] of editor.document.text!.split('\n').entries()) {
-                let m: RegExpExecArray | null
-                do {
-                    m = STATSD_PATTERN.exec(line)
-                    if (m) {
-                        decorations.push({
-                            range: new sourcegraph.Range(i, 0, i, 0),
-                            isWholeLine: true,
-                            after: {
-                                backgroundColor: '#774b9e',
-                                color: 'rgba(255, 255, 255, 0.8)',
-                                contentText: ' View metric (Datadog) » ',
-                                linkURL: buildUrl(m[1]).toString(),
-                            },
-                        })
-                    }
-                } while (m)
-                STATSD_PATTERN.lastIndex = 0 // reset
+function decorateEditor(editor: sourcegraph.CodeEditor): void {
+    const decorations: sourcegraph.TextDocumentDecoration[] = []
+    const decorationType = sourcegraph.app.createDecorationType()
+    for (const [i, line] of editor.document.text!.split('\n').entries()) {
+        let m: RegExpExecArray | null
+        do {
+            m = STATSD_PATTERN.exec(line)
+            if (m) {
+                decorations.push({
+                    range: new sourcegraph.Range(i, 0, i, 0),
+                    isWholeLine: true,
+                    after: {
+                        backgroundColor: '#774b9e',
+                        color: 'rgba(255, 255, 255, 0.8)',
+                        contentText: ' View metric (Datadog) » ',
+                        linkURL: buildUrl(m[1]).toString(),
+                    },
+                })
             }
-
-            if (sourcegraph.app.activeWindowChanges) {
-                const activeEditor = from(sourcegraph.app.activeWindowChanges).pipe(
-                    filter((window): window is sourcegraph.Window => window !== undefined),
-                    switchMap(window => window.activeViewComponentChanges),
-                    filter((editor): editor is sourcegraph.CodeEditor => editor !== undefined)
-                )
-
-                // When the configuration or current file changes, publish new decorations.
-                const decorationType = sourcegraph.app.createDecorationType()
-
-                context.subscriptions.add(
-                    activeEditor.subscribe(editor => {
-                        editor.setDecorations(decorationType, decorations)
-                    })
-                )
-            }
-        }
+        } while (m)
+        STATSD_PATTERN.lastIndex = 0 // reset
     }
-    sourcegraph.workspace.onDidOpenTextDocument.subscribe(() =>
-        decorateEditors(sourcegraph.app.activeWindow!.visibleViewComponents)
+    editor.setDecorations(decorationType, decorations)
+}
+
+export function activate(context: sourcegraph.ExtensionContext): void {
+    const activeEditor = from(sourcegraph.app.activeWindowChanges).pipe(
+        filter((window): window is sourcegraph.Window => window !== undefined),
+        switchMap(window => window.activeViewComponentChanges),
+        filter((editor): editor is sourcegraph.CodeEditor => editor !== undefined)
     )
+
+    if (sourcegraph.app.activeWindowChanges) {
+        // When the active editor changes, publish new decorations.
+        context.subscriptions.add(activeEditor.subscribe(decorateEditor))
+    }
 }
 
 function buildUrl(metricName: string): URL {
